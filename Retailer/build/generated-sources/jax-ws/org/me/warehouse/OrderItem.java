179ba118048ca693697e2c853302617cf8be1a11
arnaudrivard <arnaud.rivard@gmail.com>
2014-03-20 19:10:23 -0400

    commit all the things!!

---
+++ Retailer/build/generated-sources/jax-ws/org/me/warehouse/OrderItem.java
@@ -0,0 +1,80 @@
+
+package org.me.warehouse;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+import org.me.manufacturer.Product;
+
+
+/**
+ * <p>Java class for orderItem complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="orderItem">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="product" type="{http://manufacturer.me.org/}product" minOccurs="0"/>
+ *         &lt;element name="quantity" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "orderItem", propOrder = {
+    "product",
+    "quantity"
+})
+public class OrderItem {
+
+    protected Product product;
+    protected int quantity;
+
+    /**
+     * Gets the value of the product property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Product }
+     *     
+     */
+    public Product getProduct() {
+        return product;
+    }
+
+    /**
+     * Sets the value of the product property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Product }
+     *     
+     */
+    public void setProduct(Product value) {
+        this.product = value;
+    }
+
+    /**
+     * Gets the value of the quantity property.
+     * 
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * Sets the value of the quantity property.
+     * 
+     */
+    public void setQuantity(int value) {
+        this.quantity = value;
+    }
+
+}
---
+++ Retailer/src/java/org/me/retailer/RetailerWS.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.me.retailer;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.xml.ws.WebServiceRef;
+import org.me.warehouse.*;
+import org.me.warehouse.WarehouseWS_Service;
+
+/**
+ *
+ * @author arnaud.rivard
+ */
+@WebService(serviceName = "RetailerWS")
+public class RetailerWS {
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/ec2-54-186-163-110.us-west-2.compute.amazonaws.com_8080/Warehouse1/WarehouseWS.wsdl")
+    private WarehouseWS_Service service;
+    private List<OrderItem> itemsShipped;
+    private List<OrderItem> itemsLeft;
+    private List<OrderItem> itemsTemp;
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "submitOrder")
+    public List<OrderItem> submitOrder(@WebParam(name = "orderItemList") List<OrderItem> orderItemList) {
+      //TODO ADD ITEM TO SHIP GOODS  shipGoods = new shipGoods; 
+      itemsLeft = orderItemList;
+      itemsTemp = new ArrayList();
+      itemsShipped = new ArrayList();
+      itemsTemp = shipGoods1(itemsLeft);
+      itemsLeft.removeAll(itemsTemp);
+      itemsShipped.addAll(itemsTemp);
+//      itemsTemp = shipGoods2(itemsLeft);
+//      itemsLeft.removeAll(itemsTemp);
+//      itemsShipped.addAll(itemsTemp);
+//      itemsTemp = shipGoods3(itemsLeft);
+//      itemsLeft.removeAll(itemsTemp);
+//      itemsShipped.addAll(itemsTemp);
+    
+     return itemsShipped;
+     }
+
+    private List<OrderItem> shipGoods1(List<OrderItem> items){
+        org.me.warehouse.WarehouseWS port = service.getWarehouseWSPort();
+        return port.shipGoods(items);
+    }
+    
+ /*  private List<OrderItem> shipGoods2(List<OrderItem> items){
+        org.me.warehouse.WarehouseWS port = service.getWarehouseWSPort();
+        return port.shipGoods(items);
+    }
+    private List<OrderItem> shipGoods3(List<OrderItem> items){
+        org.me.warehouse.WarehouseWS port = service.getWarehouseWSPort();
+        return port.shipGoods(items);
+    }
+  */
+
+}
